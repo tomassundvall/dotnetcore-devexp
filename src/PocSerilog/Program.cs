54add0433b0d3d5a8fbc69d80785cfc3857cf875
= <=>
2018-06-26 11:22:22 +0200

    Added SerilogPOC project

---
+++ src/PocSerilog/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+namespace TSundvall.DotnetCoreDevExp.PocSerilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File(new CompactJsonFormatter(), "log.clef")
+            .CreateLogger();
+
+            Log.Information("Logging an item number {ItemNumber}", 250);
+
+            Log.CloseAndFlush();
+        }
+    }
+}
